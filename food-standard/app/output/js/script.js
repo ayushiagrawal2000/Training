1bcc5a3fbf28850f8b11719e41363b65d06d267f
ayushi <ayushi@newput.com>
2021-09-03 11:26:34 +0530

    fixed border and optimize js

--- food-standard/app/output/js/script.js
+++ food-standard/app/output/js/script.js
@@ -19,7 +19,7 @@ var defaultHtml2 = $('.blocks-section2').html();
 var sectionDetails = $('.details-container').html();
 
 function onHover() {
-    $(".chart-section-container").css("border", "2px solid black");
+    $(".chart-section-container").css("border", "3px solid black");
     $(".cor p").text("description");
     $(".cor").css({"background":"#EDEDED", "color":"black"});
     $('.blocks-section1').html("<div><div><b>Title</b></div><div>some description about the title is gonna be here</div></div>");
@@ -31,7 +31,7 @@ function onHoverOut() {
     console.log("inside out function");
     $(".cor p").text(defaultValue);
     $(".cor").css({"background":"black", "color":"white"});
-    $(".chart-section-container").css("border", "2px solid white");
+    $(".chart-section-container").css("border", "3px solid white");
     $('.blocks-section1').html(defaultHtml);
     $('.blocks-section1').css("display","flex");
     $('.blocks-section2').html(defaultHtml2);
@@ -44,85 +44,66 @@ $(".cor").hover(function(){
 });
 
 //chart-section2 hover effect
-var purchasesDefaultTitle = $(".purchase").text();
+var purchasesDefaultTitle = $(".purchase p").text();
 var chartsDefault = $(".charts-wrapper").html();
-function purchaseTitleHoverOut() {
-    $('.purchase p').text(purchasesDefaultTitle);
-    $('.purchase').css({"background":"black", "color":"white"});
-    $(".purchases-container").css("border", "2px solid white");
+function purchaseTitleOnHover(text, background, color, border) {
+    $('.purchase p').text(text);
+    $('.purchase').css({"background":background, "color":color});
+    $(".purchases-container").css("border", border);
 }
 
-function purchaseTitleOnHover() {
-    $('.purchases-container').css("border","2px solid black");
-    $('.purchase p').text("description");
-    $('.purchase').css({"background":"#EDEDED", "color":"black"});
-}
-var description = `some description about the purchasing chart
+var chartDescription = `some description about the purchasing chart
 description about the purch description about the purch`;
-function purchaseChartOnHover() {
-    $('.charts-wrapper').html(description);
-    $('.charts-wrapper').css({"padding":"10px","font-size":"10px"});
-}
-
-function purchaseChartHoverOut() {
-    $('.charts-wrapper').html(chartsDefault);
-    $('.charts-wrapper').css("padding","0px");
+function purchaseChartOnHover(text,padding,font) {
+    $('.charts-wrapper').html(text);
+    $('.charts-wrapper').css({"padding":padding,"font-size":font});
 }
 
 $('.purchase').hover(function() {
-    purchaseTitleOnHover();
+    purchaseTitleOnHover("description","#EDEDED","black","3px solid black");
 },function(){
-    purchaseTitleHoverOut();
+    purchaseTitleOnHover(purchasesDefaultTitle,"black","white","3px solid white");
 });
 
 $('.purchase-chart').hover(function() {
-   purchaseChartOnHover();
+    purchaseChartOnHover(chartDescription,"10px","10px");
 },function(){
-    purchaseChartHoverOut();
+    purchaseChartOnHover(chartsDefault,"0px");
 });
 
 //chart-section3 hover effect
 
-var text = `this is judt for testing here we gonna put some discription`
+var supplierText = `this is judt for testing here we gonna put some discription`
 var suppliersDefaultTitle = $('.supplier p').text();
 var amountDefaultValue1 = $('.amount-value2').text();
 var amountDefaultValue2 = $('.amount-value2').text();
-function supplierTitleOnHover() {
-    $('.suppliers').css("border","2px solid black");
+function supplierTitleOnHover(text, border,background,color) {
+    $('.suppliers').css("border",border);
     $('.supplier p').text(text);
-    $('.supplier').css({"background":"#EDEDED", "color":"black"});
+    $('.supplier').css({"background":background, "color":color});
 }
-function supplierTitleHoverOut() {
-    $(".supplier p").text(suppliersDefaultTitle);
-    $(".supplier").css({"background":"black", "color":"white"});
-    $(".suppliers").css("border", "2px solid white");
-}
+
 $('.supplier').hover(function() {
-    supplierTitleOnHover();
+    supplierTitleOnHover(supplierText, "3px solid black", "#EDEDED", "black");
 },function(){
-    supplierTitleHoverOut();
+    supplierTitleOnHover(suppliersDefaultTitle, "3px solid white", "black", "white");
 });
 
-function amountSection1OnHover(value, color, size) {
-    $('.amount-value1').text(value);
-    $('.amount-value1').css({"font-size":size,"color":color});
-}
-
-function amountSection2OnHover(value, color, size) {
-    $('.amount-value2').text(value);
-    $('.amount-value2').css({"font-size":size,"color":color});
+function amountSectionOnHover(className, value, color, size) {
+    $(className).text(value);
+    $(className).css({"font-size":size,"color":color});
 }
 
 $('.amount-value1').hover(function() {
-    amountSection1OnHover("description","black","10px");
+    amountSectionOnHover(".amount-value1", "description", "black", "10px");
 },function() {
-    amountSection1OnHover(amountDefaultValue1,"#E35E56","25px")
+    amountSectionOnHover(".amount-value1", amountDefaultValue1, "#E35E56", "25px");
 });
 
 $('.amount-value2').hover(function() {
-    amountSection2OnHover("description","black","10px");
+    amountSectionOnHover(".amount-value2","description","black","10px");
 },function() {
-    amountSection2OnHover(amountDefaultValue2,"#63BF87","25px");
+    amountSectionOnHover(".amount-value2",amountDefaultValue2,"#63BF87","25px");
 });
 
 var purchasingDefaultHtml = $('.purchasing-charts-container').html();
@@ -137,6 +118,7 @@ $('.purchasing-data').hover(function() {
 });
 
 //toggle effect for mobile view section 1
+
 var media = window.matchMedia("(max-width: 500px)")
 if(media.matches) {
     $('.cor').unbind('mouseenter mouseleave');
@@ -144,9 +126,10 @@ if(media.matches) {
     $('.purchase-chart').unbind('mouseenter mouseleave');
     $('.supplier').unbind('mouseenter mouseleave');
     $('.amount-value1').unbind('mouseenter mouseleave');
-    $('amount-value2').unbind('mouseenter mouseleave');
-    $('purchasing-data').unbind('mouseenter mouseleave');
+    $('.amount-value2').unbind('mouseenter mouseleave');
+    $('.purchasing-data').unbind('mouseenter mouseleave');
 }
+
 var chartDetails = $('.charts-container').html();
 var count = 0;
 $('.cor-icon').click(function(){
@@ -175,45 +158,45 @@ $('.details-icon').click(function() {
 $('.purchase-title-info').click(function(){
     count++;
     if(count % 2 == 0) {
-        purchaseTitleHoverOut();
+        purchaseTitleOnHover(purchasesDefaultTitle,"black","white","3px solid white");
     }else {
-        purchaseTitleOnHover();
+        purchaseTitleOnHover("description","#EDEDED","black","3px solid black");
     }
 });
 
 $('.purchase-chart-info').click(function() {
     count++;
     if(count % 2 == 0) {
-        purchaseChartHoverOut();
+        purchaseChartOnHover(chartsDefault,"0px");
     }else {
-        purchaseChartOnHover();
+        purchaseChartOnHover(chartDescription,"10px","10px");
     }
 });
 
 $('.supplier-title-info').click(function() {
     count++;
     if(count % 2 == 0) {
-        supplierTitleHoverOut();
+        supplierTitleOnHover(suppliersDefaultTitle, "3px solid white", "black", "white");
     }else {
-        supplierTitleOnHover();
+        supplierTitleOnHover(supplierText, "3px solid black", "#EDEDED", "black");
     }
 });
 
 $('.amount-section1-info').click(function() {
     count++;
     if(count % 2 == 0) {
-        amountSection1OnHover(amountDefaultValue1,"#E35E56","25px");
+        amountSectionOnHover(".amount-value1", amountDefaultValue1, "#E35E56", "25px");
     }else {
-        amountSection1OnHover("description","black","10px");
+        amountSectionOnHover(".amount-value1", "description", "black", "10px");
     }
 });
 
 $('.amount-section2-info').click(function() {
     count++;
     if(count % 2 == 0) {
-        amountSection2OnHover(amountDefaultValue2,"#63BF87","25px");
+        amountSectionOnHover(".amount-value2",amountDefaultValue2,"#63BF87","25px");
     }else {
-        amountSection2OnHover("description","black","10px");
+        amountSectionOnHover(".amount-value2","description","black","10px");
     }
 });
 
